8636cf4a90b11b9295ad82826fb07b2816097aa3
sanwenyukaochi <sanwenyukaochi@outlook.com>
2025-10-22 16:13:00 +0800

    feat: 新增实体类

---
+++ creator-data-management/src/main/java/com/data/creator/repositories/InfluencerRepository.java
@@ -0,0 +1,9 @@
+package com.data.creator.repositories;
+
+import com.data.creator.entities.Influencer;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface InfluencerRepository extends CrudRepository<Influencer, Long> {
+}
